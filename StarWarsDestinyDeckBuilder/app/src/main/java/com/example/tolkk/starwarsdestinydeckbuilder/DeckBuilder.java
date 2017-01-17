667838424b33b461f79e9780c29d2f15180e7e08
tuomashp <tuomas.patola@cs.helsinki.fi>
2017-01-17 20:54:36 +0200

    lisää

--- StarWarsDestinyDeckBuilder/app/src/main/java/com/example/tolkk/starwarsdestinydeckbuilder/DeckBuilder.java
+++ StarWarsDestinyDeckBuilder/app/src/main/java/com/example/tolkk/starwarsdestinydeckbuilder/DeckBuilder.java
@@ -25,7 +25,7 @@ public class DeckBuilder extends AppCompatActivity {
      */
     private static final boolean AUTO_HIDE = true;
 
-    /**
+    /** 
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
      */
